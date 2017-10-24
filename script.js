40357f7265a18b009852522fe295ecf970f36be1
AddictiveScience <Jess@addictivescience.com>
2017-10-23 20:28:23 -0400

    added things

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var app = express();
+var cypher = require('cypher-stream')('bolt://34.228.185.57/', 'neo4j', 'Gateway2');
 
+var data = '';
 
 app.get('/ancient_places', function(req, res) {
-
-  res.send("hello world");
+  cypher('match (ancient_places:AncientPlaces) return ancient_places')
+  .on('data', function(result) {
+    data += JSON.stringify(result);
+  })
+  .on('end', function() {
+    console.log("DATA");
+    console.log(data);
+    res.send(data);
+    data = '';
+  });
 });
 
 app.listen(3000, function(){
